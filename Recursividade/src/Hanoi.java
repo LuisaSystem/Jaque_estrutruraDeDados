36c797d44ef40adfda2abb80f30d1c219fb07c0f
LUISA COELHO <1090482521003@fatec-srv.local>
2026-09-03 08:20:51 -0300

    torre de hanoi

--- Recursividade/src/Hanoi.java
+++ Recursividade/src/Hanoi.java
@@ -1,7 +1,15 @@
 public class Hanoi {
-    public static int hanoi(int tor1, int tor2, int tor3){
-        if (){
-
+    public static void hanoi(int n, String origem, String destino, String aux){
+        if (n == 1){
+            System.out.println("Mova disco do pino "+origem+" para o pino "+destino);
+        }else {
+            hanoi(n-1, origem, aux, destino);
+            System.out.println("Mova disco do pino "+origem+" para o pino "+destino);
+            hanoi(n-1,aux, destino, origem);
         }
+    }
+
+    public static void main(String[] args) {
+        hanoi(4,"A","B", "C");
     }
 }
