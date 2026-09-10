f77fc98e2886e1d86a6f619a75756ef0393d552e
LUISA COELHO <1090482521003@fatec-srv.local>
2026-09-10 10:34:44 -0300

    buscas binarias e squencial

---
+++ Buscas/src/SequencialBi.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+
+public class SequencialBi {
+
+    //busca sequencial
+    public static void sequencial(int[] vet){
+        int aux;
+        for (int i = 0; i < vet.length; i++) {
+            int x = 0;
+            if (vet[i] == x){
+                aux = x;
+                vet[i] = aux;
+                System.out.println(Arrays.toString(vet));
+            }
+        }
+    }
+
+    //bunca binária - recursiva
+    public static int busca(int[] vet, int i, int f, int x){
+        int meio;
+        if (i > f){
+            return -1;
+        }else{
+            meio = (i + f) / 2;
+            if (vet[meio] == x){
+                return meio;
+            } else if (x < vet[meio]) {
+                return busca(vet, i, meio-1,x);
+            }else {
+                return busca(vet, meio+1, f, x);
+            }
+
+        }
+    }
+
+    //busca binária - não recursiva
+    public static void buscaII(int[] vet, int i, int f, int x){
+        int meio;
+        if (i > f){
+            System.out.println("no");
+        }else {
+            meio = (i + f)/2;
+            if (vet[meio] == x){
+                System.out.println(meio);
+            } else if (x < vet[meio]) {
+                System.out.println("Resul1: "+ Arrays.toString(vet)+", "+i+", "+(meio-1)+", "+x+".");
+            }else {
+                System.out.println("Resul2: "+ Arrays.toString(vet)+", "+(meio+1)+", "+f+", "+x+".");
+            }
+        }
+    }
+
+
+
+    // tamanho do vetor
+    // 1. 10
+    public static void number(int[] vet){
+        int j = 0;
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = j;
+            j+=2;
+        }
+    }
+    public static final int tam = 10;
+
+    public static void main(String[] args) {
+        int[] p = new int[tam] ;
+        number(p);
+
+        //SEQUENCIAL
+        long fim = System.nanoTime();
+        long inicio = System.nanoTime();
+        long tempo = fim - inicio;
+        double dura = tempo/1000000.0;
+        sequencial(p);
+        System.out.println(Arrays.toString(p));
+        System.out.println("tempo: "+dura);
+
+        System.out.println("================================");
+
+        //BUSCA - RECURSIVA
+        fim = System.nanoTime();
+        inicio = System.nanoTime();
+        tempo = fim - inicio;
+        dura = tempo/1000000.0;
+        busca(p, 0, p.length-1, 9);
+        System.out.println(Arrays.toString(p));
+        System.out.println("tempo: "+dura);
+
+        System.out.println("================================");
+
+        //BUSCA - NÂO RECURSIVA
+        fim = System.nanoTime();
+        inicio = System.nanoTime();
+        tempo = fim - inicio;
+        dura = tempo/1000000.0;
+        buscaII(p,0, p.length-1, 9);
+        System.out.println(Arrays.toString(p));
+        System.out.println("tempo: "+dura);
+
+    }
+}
